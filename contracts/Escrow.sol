6db01a08603202b52b8807f90149890f46cebf7b
Jakub Smekal <jakub.smekal@gmail.com>
2021-06-03 13:24:41 +0100

    FIX: wrong declaration of countdown
    In constructor, I accidentally declared countdownLength, which not only
    posed a problem as I have declared once before, but I also used a
    variable of a different name which remained undeclared. This is fixed
    now and I renamed the second countdown declaration

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -95,7 +95,7 @@ contract Escrow is Countdown, SafeMath {
     if (agreementParams.length != 0) {
       (
         uint256 ratio,
-        uint256 countdownLength
+        uint256 agreementCountdown
       ) = abi.decode(agreementParams, (uint256, uint256));
       require(ratio == uint256(uint120(ratio)), "ratio out of bounds");
       require(agreementCountdown == uint256(uint128(agreementCountdown)), "agreementCountdown out of bounds");
