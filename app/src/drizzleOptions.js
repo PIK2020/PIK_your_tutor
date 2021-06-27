8b794c34e2bc5f9fe246af4c5bb44046e37d6a32
Jakub Smekal <jakub.smekal@gmail.com>
2021-06-27 14:11:43 +0100

    configured drizzleOptions.js

--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -1,16 +1,16 @@
 import Web3 from "web3";
-import ComplexStorage from "./contracts/ComplexStorage.json";
-import SimpleStorage from "./contracts/SimpleStorage.json";
-import TutorialToken from "./contracts/TutorialToken.json";
+import Escrow from "./contracts/Escrow.json";
 
 const options = {
   web3: {
-    block: false,
-    customProvider: new Web3("ws://localhost:8545"),
+    fallback: {
+        type: "ws",
+        url: "ws://127.0.0.1:9545",
+    },
   },
-  contracts: [SimpleStorage, ComplexStorage, TutorialToken],
+  contracts: [Escrow],
   events: {
-    SimpleStorage: ["StorageSet"],
+    Escrow: ["Initialized", "Finalized", "StakeDeposited", "PaymentDeposited", "Cancelled", "Ended"],
   },
 };
 
